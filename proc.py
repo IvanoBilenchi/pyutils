59c4bb6af36a789508e89016248e0b0f53c7e9d7
Ivano Bilenchi <ivanobilenchi@gmail.com>
2017-08-18 19:07:54 +0200

    Major refactoring of the 'proc' module

--- proc.py
+++ proc.py
@@ -1,3 +1,5 @@
+import errno
+import re
 import os
 import subprocess
 import threading
@@ -5,84 +7,293 @@ from distutils import spawn
 
 import exc
 from decorators import memoized
+from enum import Enum
 
 
 # Public classes
 
 
-class CallResult(object):
-    """Contains information about a called process."""
+class Task(object):
+    """Spawn processes and easily capture their output."""
 
-    def __init__(self, proc_name, exit_code, stdout=None, stderr=None):
+    @property
+    def name(self):
+        """:rtype : str"""
+        return os.path.basename(self._path)
+
+    @property
+    def path(self):
+        """:rtype : str"""
+        return self._path
+
+    @property
+    def args(self):
+        """:rtype : list[str]"""
+        return self._args
+
+    @property
+    def output_action(self):
+        """:rtype : OutputAction"""
+        return self._output_action
+
+    @property
+    def stdout(self):
+        """:rtype : str"""
+        return self._stdout
+
+    @property
+    def stderr(self):
+        """:rtype : str"""
+        return self._stderr
+
+    @property
+    def exit_code(self):
+        """:rtype : int"""
+        return self._exit_code
+
+    def __init__(self, executable, args=None, output_action=OutputAction.STORE):
         """
-        :param str proc_name : Name of the called process.
+        :param str executable : The process executable.
+        :param list[str] args : The process args.
+        :param OutputAction output_action : Output action.
+        """
+        exc.raise_if_falsy(executable=executable, output_action=output_action)
+
+        if not os.path.isabs(executable):
+            executable = find_executable(executable)
+
+        self._path = executable
+        self._args = args
+        self._output_action = output_action
+
+        self._stdout = None
+        """:type : str"""
+
+        self._stderr = None
+        """:type : str"""
+
+        self._exit_code = None
+        """:type : int"""
+
+        self._process = None
+        """:type : subprocess.Popen"""
+
+    def run(self, timeout=None):
+        """Runs the task.
+
+        :param float timeout : Timeout (s).
+        """
+        event = None
+        stdout = None
+        stderr = None
+
+        try:
+            args = [self.path]
+            args.extend(self.args)
+
+            if self.output_action == OutputAction.DISCARD:
+                with open(os.devnull, 'w') as null_file:
+                    self._process = subprocess.Popen(args, stdout=null_file, stderr=null_file)
+            elif self.output_action == OutputAction.STORE:
+                self._process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=-1)
+            else:
+                self._process = subprocess.Popen(args)
+
+            if timeout is not None:
+                event = self._watchdog(timeout)
+
+            if self.output_action == OutputAction.STORE:
+                out, err = self._process.communicate()
+                stdout = out.strip()
+                stderr = err.strip()
+            else:
+                self._process.wait()
+        except Exception as e:
+            exc.re_raise_new_message(e, 'Failed to call process: {}'.format(self.path))
+        finally:
+            if event:
+                if event.is_set():
+                    # Timeout occurred
+                    raise WatchdogException
+                else:
+                    # Process completed successfully, stop the watchdog
+                    event.set()
+
+            self._complete(self._process.returncode, stdout, stderr)
+
+    def run_async(self, timeout=None, exit_handler=None):
+        """Runs the task asynchronously.
+
+        :param float timeout : Timeout (s).
+        :param (Process, Exception) -> None exit_handler : Exit handler.
+        """
+        bg_proc = threading.Thread(target=self._call_async_body, args=[timeout, exit_handler])
+        bg_proc.start()
+
+    def raise_if_failed(self, ensure_output=False, message=None):
+        """Raise an IOError if the task returned with a non-zero exit code.
+
+        :param bool ensure_output : If True, an exception is raised if stdout is empty.
+        :param str message : Optional message to prepend to the generated error message.
+        """
+        auto_msg = None
+        should_raise = False
+
+        if self.exit_code:
+            auto_msg = 'Process "{}" returned exit code: {:d}'.format(self.name, self.exit_code)
+            should_raise = True
+        elif ensure_output and not self.stdout:
+            auto_msg = 'Process "{}" returned no output.'.format(self.name)
+            should_raise = True
+
+        if should_raise:
+            err_lines = []
+            for msg in [message, auto_msg, self.stderr]:
+                if msg:
+                    err_lines.append(msg)
+
+            raise IOError('\n'.join(err_lines))
+
+    # Protected methods
+
+    def _complete(self, exit_code, stdout, stderr):
+        """
         :param int exit_code : Exit code.
-        :param str stdout : Standard output.
-        :param str stderr : Standard error.
+        :param str stdout : Standard output returned by the task.
+        :param str stderr : Standard error returned by the task.
         """
-        exc.raise_if_falsy(proc_name=proc_name)
         exc.raise_if_none(exit_code=exit_code)
 
-        self.proc_name = proc_name
-        self.exit_code = exit_code
-        self.stdout = stdout
-        self.stderr = stderr
+        self._exit_code = exit_code
+        self._stdout = stdout
+        self._stderr = stderr
+
+        self._process = None
+
+    def _call_async_body(self, timeout=None, exit_handler=None):
+        """
+        :param float timeout : Timeout (s).
+        :param (Process, Exception) -> None exit_handler : Exit handler.
+        """
+        err = None
+
+        try:
+            self.run(timeout=timeout)
+        except Exception as e:
+            err = e
+        finally:
+            if exit_handler:
+                exit_handler(self, err)
+
+    def _watchdog(self, timeout):
+        """Kills process after a certain timeout.
+
+        :param float timeout : Timeout (s).
+
+        :rtype : threading.Event
+        :return Event that should be set by the caller when the process has finished running.
+        """
+        event = threading.Event()
+        watchdog_thread = threading.Thread(target=self._kill_after_timeout, args=[event, timeout])
+        watchdog_thread.start()
+        return event
+
+    def _kill_after_timeout(self, event, timeout):
+        """
+        :param threading.Event event : Shared event.
+        :param float timeout : Timeout (s).
+        """
+        event.wait(timeout)
+
+        if not event.is_set():
+            self._process.kill()
+            event.set()
 
 
-class OutputAction:
-    """Pseudo-enum class for output actions. Do not instantiate."""
-    # These declarations could be avoided, but are useful for PyCharm code completion.
+class Jar(Task):
+    """Spawn jars and easily capture their output."""
+
+    def __init__(self, jar, jar_args=None, vm_opts=None, output_action=OutputAction.STORE):
+        """
+        :param str jar : The jar to execute.
+        :param list[str] jar_args : Args that should be passed to the jar.
+        :param list[str] vm_opts : Java VM options.
+        :param OutputAction output_action : Output action.
+        """
+        exc.raise_if_falsy(jar=jar, output_action=output_action)
+
+        args = []
+
+        if vm_opts:
+            args.extend(vm_opts)
+
+        args.extend(['-jar', jar])
+
+        if jar_args:
+            args.extend(jar_args)
+
+        super(Jar, self).__init__(executable='java', args=args, output_action=output_action)
+
+
+class Benchmark(object):
+    """Runs benchmarks for a given task."""
+
+    @property
+    def task(self):
+        """:rtype : Task"""
+        return self._task
+
+    @property
+    def max_memory(self):
+        """:rtype : long"""
+        return self._max_memory
+
+    def __init__(self, task):
+        """
+        :param Task task : The task to benchmark.
+        """
+        exc.raise_if_none(task=task)
+
+        self._task = task
+        self._max_memory = 0
+
+    def run(self, timeout=None):
+        """Runs the benchmark.
+
+        :param float timeout : Timeout (s).
+        """
+        time_task = Task('time', args=['-lp'] + self.task.args)
+        time_task.run(timeout=timeout)
+
+        stderr = time_task.stderr
+        exc.raise_if_falsy(stderr=stderr)
+
+        idx = stderr.rfind('real ')
+
+        if idx < 0:
+            exc.raise_ioerror(errno.ENODATA, message='Benchmark failed.')
+
+        time_output = stderr[idx:]
+        result = re.search(r'[ ]*([0-9]+)[ ]+maximum resident set size', time_output)
+        exc.raise_if_falsy(result=result)
+
+        self._max_memory = long(result.group(1))
+
+        # noinspection PyProtectedMember
+        self._task._complete(time_task.exit_code, time_task.stdout, stderr[:idx])
+
+
+class OutputAction(Enum):
+    """Pseudo-enum class for output actions."""
     PRINT = None
     DISCARD = None
-    RETURN = None
-
-    def __init__(self, name):
-        self.name = name
-
-    def __repr__(self):
-        return '<OutputAction: {}>'.format(self.name)
-
-for action_str in ['PRINT', 'DISCARD', 'RETURN']:
-    setattr(OutputAction, action_str, OutputAction(action_str))
+    STORE = None
+OutputAction.init()
 
 
 class WatchdogException(Exception):
     """Raised when a process times out."""
     pass
-
-
-# Exceptions
-
-
-def raise_if_failed(call_result, ensure_output=False, message=None):
-    """Raise an IOError if the process returned with a non-zero exit code.
-
-    :param CallResult call_result : Call result of the called process.
-    :param bool ensure_output : If True, an exception is raised if stdout is empty.
-    :param str message : Optional message to prepend to the generated error message.
-    """
-    exc.raise_if_none(call_result=call_result)
-
-    auto_msg = None
-    stderr = None
-    should_raise = False
-
-    if call_result.exit_code:
-        auto_msg = 'Process "{}" returned exit code: {:d}'.format(call_result.proc_name, call_result.exit_code)
-        stderr = call_result.stderr
-        should_raise = True
-    elif ensure_output and not call_result.stdout:
-        auto_msg = 'Process "{}" returned no output.'.format(call_result.proc_name)
-        stderr = None
-        should_raise = True
-
-    if should_raise:
-        err_lines = []
-        for msg in [message, auto_msg, stderr]:
-            if msg:
-                err_lines.append(msg)
-
-        raise IOError('\n'.join(err_lines))
 
 
 # Public functions
@@ -106,73 +317,6 @@ def find_executable(executable, path=None):
     return exe_path
 
 
-def call(args, output_action=OutputAction.RETURN, timeout=None):
-    """Call process and return its exit code and output.
-
-    :param list args : Process arguments.
-    :param proc.OutputAction output_action : PRINT, DISCARD or RETURN.
-    :param float timeout : Timeout (s).
-    :rtype : CallResult
-    :return Call result object. stdout and stderr attributes are only populated
-        if output_action == OutputAction.RETURN.
-    """
-    exc.raise_if_falsy(args=args, output_action=output_action)
-
-    out = None
-    err = None
-    process = None
-    event = None
-
-    try:
-        if output_action == OutputAction.DISCARD:
-            with open(os.devnull, 'w') as null_file:
-                process = subprocess.Popen(args, stdout=null_file, stderr=null_file)
-        elif output_action == OutputAction.RETURN:
-            process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=-1)
-        else:
-            process = subprocess.Popen(args)
-
-        if timeout is not None:
-            event = __watchdog(process, timeout)
-
-        if output_action == OutputAction.RETURN:
-            out, err = process.communicate()
-            out = out.strip()
-            err = err.strip()
-        else:
-            process.wait()
-    except Exception as e:
-        exc.re_raise_new_message(e, 'Failed to call process: {}'.format(args[0]))
-    finally:
-        if event:
-            if event.is_set():
-                # Timeout occurred
-                raise WatchdogException
-            else:
-                # Process completed successfully, stop the watchdog
-                event.set()
-
-    return CallResult(args[0], process.returncode, out, err)
-
-
-def call_background(args, quiet=False):
-    """Call process in background.
-
-    :param list[str] args : Process arguments.
-    :param bool quiet : If True the output is discarded, else it is printed.
-    """
-    exc.raise_if_falsy(args=args)
-
-    try:
-        if quiet:
-            with open(os.devnull, 'w') as null_file:
-                subprocess.Popen(args, stdout=null_file, stderr=null_file)
-        else:
-            subprocess.Popen(args)
-    except Exception as e:
-        exc.re_raise_new_message(e, 'Failed to call process: {}'.format(args[0]))
-
-
 def killall(process, signal=None):
     """killall command wrapper function.
 
@@ -183,14 +327,16 @@ def killall(process, signal=None):
     """
     exc.raise_if_falsy(process=process)
 
-    args = [find_executable('killall')]
+    args = []
+
     if signal:
         args.append('-' + str(signal))
     args.append(process)
 
-    exit_code = call(args, OutputAction.DISCARD).exit_code
+    task = Task('killall', args=args, output_action=OutputAction.DISCARD)
+    task.run()
 
-    return exit_code == 0
+    return task.exit_code == 0
 
 
 def pkill(pattern, signal=None, match_arguments=True):
@@ -204,45 +350,15 @@ def pkill(pattern, signal=None, match_arguments=True):
     """
     exc.raise_if_falsy(pattern=pattern)
 
-    args = [find_executable('pkill')]
+    args = []
+
     if signal:
         args.append('-' + str(signal))
     if match_arguments:
         args.append('-f')
     args.append(pattern)
 
-    exit_code = call(args, OutputAction.DISCARD).exit_code
+    task = Task('pkill', args=args, output_action=OutputAction.DISCARD)
+    task.run()
 
-    return exit_code == 0
-
-
-# Private functions
-
-
-def __watchdog(process, timeout):
-    """Kills process after a certain timeout.
-
-    :param subprocess.Popen process : Process to kill.
-    :param float timeout : Timeout (s).
-
-    :rtype : threading.Event
-    :return Event that should be set by the caller when the process has finished running.
-    """
-    event = threading.Event()
-    watchdog_thread = threading.Thread(target=__kill, args=[process, event, timeout])
-    watchdog_thread.start()
-    return event
-
-
-def __kill(process, event, timeout):
-    """Watchdog thread function.
-
-    :param subprocess.Popen process : Process to kill.
-    :param threading.Event event : Shared event.
-    :param float timeout : Timeout (s).
-    """
-    event.wait(timeout)
-
-    if not event.is_set():
-        process.kill()
-        event.set()
+    return task.exit_code == 0
