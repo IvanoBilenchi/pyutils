3774aa7d60c10181a64a9af6d3255c9b95525a11
Ivano Bilenchi <ivanobilenchi@gmail.com>
2020-09-29 16:29:54 +0200

    Use plistlib for plist manipulation

--- pyutils/io/plist.py
+++ pyutils/io/plist.py
@@ -1,81 +1,30 @@
-import objc
-
-# noinspection PyUnresolvedReferences
-from Foundation import (
-    NSData,
-    NSDataWritingAtomic,
-    NSError,
-    NSPropertyListBinaryFormat_v1_0,
-    NSPropertyListMutableContainersAndLeaves,
-    NSPropertyListOpenStepFormat,
-    NSPropertyListSerialization,
-    NSPropertyListXMLFormat_v1_0
-)
-from enum import Enum
+import plistlib
 from typing import Dict
 
 from pyutils import exc
 
 
-# Public classes
-
-
-class Format(Enum):
-    """Plist formats."""
-    OPENSTEP = NSPropertyListOpenStepFormat
-    XML = NSPropertyListXMLFormat_v1_0
-    BINARY = NSPropertyListBinaryFormat_v1_0
-
-
 # Public functions
 
 
-def read(plist_path: str) -> Dict:
-    """Read a plist file and return its contents as a dictionary."""
-    exc.raise_if_falsy(plist_path=plist_path)
-    data, error = NSData.dataWithContentsOfFile_options_error_(plist_path, 0, objc.nil)
+def read(path: str) -> Dict:
+    """Reads a plist file and returns its contents as a dictionary."""
+    exc.raise_if_falsy(path=path)
 
-    if not data:
-        msg = 'Failed to load plist file at path: {}'.format(plist_path)
-        _raise_ioerror_from_nserror(error, msg)
-
-    contents, dummy, error = NSPropertyListSerialization.propertyListWithData_options_format_error_(
-        data, NSPropertyListMutableContainersAndLeaves, objc.nil, objc.nil
-    )
-
-    if not contents:
-        msg = 'Failed to deserialize plist at path: {}'.format(plist_path)
-        _raise_ioerror_from_nserror(error, msg)
-
-    return contents
+    with open(path, 'rb') as plist_file:
+        return plistlib.load(plist_file)
 
 
-def write(plist_contents: Dict, plist_path: str, plist_format: Format = Format.BINARY) -> None:
-    """Write dictionary to a plist file."""
-    exc.raise_if_falsy(plist_contents=plist_contents,
-                       plist_path=plist_path,
-                       plist_format=plist_format)
+# noinspection PyTypeChecker
+def write(contents: Dict, path: str, binary: bool = True) -> None:
+    """Writes a dictionary to a plist file."""
+    exc.raise_if_falsy(contents=contents, path=path)
 
-    data, error = NSPropertyListSerialization.dataWithPropertyList_format_options_error_(
-        plist_contents, plist_format.value, 0, objc.nil
-    )
-
-    if not data:
-        _raise_ioerror_from_nserror(error, 'Failed to serialize plist contents.')
-
-    success, error = data.writeToFile_options_error_(plist_path, NSDataWritingAtomic, objc.nil)
-
-    if not success:
-        _raise_ioerror_from_nserror(error, 'Failed to write plist to path: {}'.format(plist_path))
+    with open(path, 'wb') as plist_file:
+        fmt = plistlib.FMT_BINARY if binary else plistlib.FMT_XML
+        plistlib.dump(contents, plist_file, fmt=fmt, sort_keys=True)
 
 
-# Private functions
-
-
-def _raise_ioerror_from_nserror(error: NSError, fallback_msg: str) -> None:
-    err_msg = None
-    if error:
-        err_msg = error.localizedDescription()
-    if not err_msg:
-        err_msg = fallback_msg
-    raise IOError(err_msg)
+def convert(path: str, binary: bool) -> None:
+    """Converts a plist file into the specified format."""
+    write(read(path), path, binary=binary)
