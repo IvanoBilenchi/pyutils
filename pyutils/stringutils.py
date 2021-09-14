0a2374caa68967403109444844e94791bb43cdd2
Ivano Bilenchi <ivanobilenchi@gmail.com>
2021-09-14 16:06:26 +0200

    Add 'string_hash' function

--- pyutils/stringutils.py
+++ pyutils/stringutils.py
@@ -1,3 +1,4 @@
+import hashlib
 import re
 from typing import List
 
@@ -13,3 +14,8 @@ def camel_case_split(string: str) -> List[str]:
 def snake_case_split(string: str) -> List[str]:
     """Splits a snake_case string."""
     return string.split('_')
+
+
+def string_hash(string: str, algo: str = 'sha1') -> str:
+    """Returns the hash of the specified string."""
+    return getattr(hashlib, algo)(string.encode('utf-8')).hexdigest()
